e362a050875e340e38b38b1da6dce31547eb6ab3
Thai Dang <thai.dang.fr@gmail.com>
2019-03-28 15:58:27 +0100

    must use a systemctl service now

--- start.sh
+++ start.sh
@@ -4,11 +4,4 @@ export scriptpath=$(realpath -s $(dirname $0))
 cd ${scriptpath}
 
 PORT=4567
-PIDFILE=${scriptpath}/dir2rss.pid
-
-if [ ! -f ${PIDFILE} ];then
-    rackup -p ${PORT} -E production -D -P ${PIDFILE}
-    echo "dir2rss started on port ${PORT}"
-else
-    echo "${PIDFILE} already exists"
-fi
+rackup -p ${PORT} -E production
